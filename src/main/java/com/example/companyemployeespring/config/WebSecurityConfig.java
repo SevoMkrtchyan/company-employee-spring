92853e0ebcd8aba029d2c3b2247b858e8f25c070
SevoMkrtchyan <sevo-mkrtchyan@mail.ru>
2021-08-21 19:29:03 +0400

    security commit

---
+++ src/main/java/com/example/companyemployeespring/config/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.companyemployeespring.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .formLogin()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.GET,"/addCompany").hasAnyRole("USER","ADMIN")
+                .antMatchers("/deleteCompany").hasAnyRole("ADMIN");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("poxos")
+                .password(encoder().encode("poxos"))
+                .roles("USER")
+                .and()
+                .withUser("petros")
+                .password(encoder().encode("petros"))
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
